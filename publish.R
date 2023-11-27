2b355f8fc0764109578404c87e536077262429ad
Vitor Marinho <vitormarinho102@gmail.com>
2023-11-27 02:20:41 -0300

    nova versão

---
+++ publish.R
@@ -0,0 +1,9 @@
+library(rsconnect)
+rsconnect::setAccountInfo(name='fjp-direi', token='75D8454C520198B1F7E42337EF04E7D9', secret='U64BAZ1StWy7cOQgQt90fa00Gu3q2xE3vtf1CubS')
+rsconnect::deployApp(appDir="C:/Users/caios/OneDrive/Área de Trabalho/dev_version/_book/", 
+                     server="posit.cloud")
+
+
+
+rsconnect::setAccountInfo(name='fjp-direi', token='75D8454C520198B1F7E42337EF04E7D9', secret='U64BAZ1StWy7cOQgQt90fa00Gu3q2xE3vtf1CubS')
+rsconnect::deployApp(appDir="C:/Users/caios/OneDrive/Área de Trabalho/dev_version/_book/", server="shinyapps.io")
